b5b6fa9ffe1fc00f825c7d41501c55222131d628
Nassertion <www.naser2002@gmail.com>
2026-03-28 11:30:11 +0300

    adding add car section

--- lib/core/constant/spaces.dart
+++ lib/core/constant/spaces.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 final dPadding = SizedBox(height: 16);
 final dPadding12 = SizedBox(height: 12);
+final dPadding8 = SizedBox(height: 8);
+final dPadding4 = SizedBox(height: 4);
--- lib/features/client/client_screen.dart
+++ lib/features/client/client_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:qitai/core/constant/colors.dart';
 import 'package:qitai/core/constant/text_styles.dart';
 import 'package:qitai/core/constant/spaces.dart';
@@ -47,12 +48,36 @@ class ClientScreen extends StatelessWidget {
                 ),
               ],
             ),
+            dPadding12,
+            Row(children: [addCar("assets/icons/addCar.svg", "اضف سيارتك")]),
             dPadding,
-            //count data
-            // ListView.builder(itemBuilder: (context, index) {}),
           ],
         ),
       ),
+    );
+  }
+
+  //if no car -> default
+  Column addCar(String img, String carName) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 64,
+          width: 64,
+          child: CircleAvatar(
+            backgroundColor: AppColors.backgroudColor,
+            child: SvgPicture.asset(img),
+          ),
+        ),
+        dPadding4,
+        Text(
+          carName,
+          style: AppTextStyles.mediumOverline.copyWith(
+            color: AppColors.primaryText,
+          ),
+        ),
+      ],
     );
   }
 }
