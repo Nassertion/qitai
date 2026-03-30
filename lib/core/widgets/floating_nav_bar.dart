f00a9203fd0910222a1b85435b0c73fe9c8c07df
Nassertion <www.naser2002@gmail.com>
2026-03-30 11:38:16 +0300

    Floating bottom nav bar

---
+++ lib/core/widgets/floating_nav_bar.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:qitai/core/constant/colors.dart';
+import 'package:qitai/core/constant/text_styles.dart';
+
+class CustomFloatingNavBar extends StatefulWidget {
+  const CustomFloatingNavBar({super.key});
+
+  @override
+  State<CustomFloatingNavBar> createState() => _CustomFloatingNavBarState();
+}
+
+class _CustomFloatingNavBarState extends State<CustomFloatingNavBar> {
+  int selected = 0;
+
+  final List<String> inactive = [
+    "assets/icons/bottom_nav_bar_icons/inactive/home.svg",
+    "assets/icons/bottom_nav_bar_icons/inactive/box.svg",
+    "assets/icons/bottom_nav_bar_icons/inactive/shopping-cart.svg",
+    "assets/icons/bottom_nav_bar_icons/inactive/profile.svg",
+  ];
+
+  final List<String> active = [
+    "assets/icons/bottom_nav_bar_icons/active/home.svg",
+    "assets/icons/bottom_nav_bar_icons/active/box.svg",
+    "assets/icons/bottom_nav_bar_icons/active/shopping-cart.svg",
+    "assets/icons/bottom_nav_bar_icons/active/profile.svg",
+  ];
+
+  final List<String> title = ["الرئيسية", "طلباتي", "سلتي", "حسابي"];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        margin: const EdgeInsets.only(bottom: 10),
+        height: 59,
+        decoration: BoxDecoration(
+          color: AppColors.backgroudColor,
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(color: const Color(0xffE7E9EA)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              blurRadius: 20,
+              offset: const Offset(0, 6),
+            ),
+          ],
+        ),
+        //temp state
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          spacing: 20,
+          children: List.generate(title.length, (index) {
+            final bool isSelected = selected == index;
+
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = index;
+                });
+              },
+              child: Material(
+                color: Colors.transparent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      isSelected ? active[index] : inactive[index],
+                    ),
+                    Text(
+                      title[index],
+                      style: isSelected
+                          ? AppTextStyles.boldOverline.copyWith(
+                              color: AppColors.primaryButton,
+                            )
+                          : AppTextStyles.regularOverline.copyWith(
+                              color: Color(0xff949CA6),
+                            ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/client/presentation/screens/client_home_screen.dart
+++ lib/features/client/presentation/screens/client_home_screen.dart
@@ -5,6 +5,7 @@ import 'package:qitai/core/constant/text_styles.dart';
 import 'package:qitai/core/constant/spaces.dart';
 import 'package:qitai/core/widgets/Page_padding.dart';
 import 'package:qitai/core/widgets/app_bar.dart';
+import 'package:qitai/core/widgets/floating_nav_bar.dart';
 import 'package:qitai/features/client/presentation/widgets/card_product.dart';
 import 'package:qitai/features/client/presentation/widgets/header.dart';
 import 'package:qitai/features/client/presentation/widgets/search.dart';
@@ -16,64 +17,75 @@ class ClientScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // bottomNavigationBar: CustomFloatingNavBar(),
       appBar: CustomeAppbar(),
       body: AppPagePadding(
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Stack(
           children: [
-            Padding8, search(),
-            dPadding,
-            sectionHeader("سياراتي"),
-            Padding12,
-            //temp
-            Row(children: [addCar("assets/icons/addCar.svg", "اضف سيارتك")]),
-            dPadding,
-            //temp
-            ClientBannerSlider(),
-            SizedBox(height: 24),
-            sectionHeader("الفئات"),
-            Padding12,
-            //temp
-            //خله يطلع من البادنق
-            SizedBox(
-              height: 95,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemCount: 8,
-                separatorBuilder: (_, __) => const SizedBox(width: 12),
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    Image.asset(
-                      'assets/images/framl.png',
-                      width: 74,
-                      height: 56,
-                    ),
-                    Padding8,
-                    Text(
-                      "الفرامل",
-                      style: AppTextStyles.mediumOverline.copyWith(
-                        color: AppColors.primaryText,
-                      ),
-                    ),
-                  ],
+            ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Padding8, search(),
+                dPadding,
+                sectionHeader("سياراتي"),
+                Padding12,
+                //temp
+                Row(
+                  children: [addCar("assets/icons/addCar.svg", "اضف سيارتك")],
                 ),
-              ),
+                dPadding,
+                //temp
+                ClientBannerSlider(),
+                SizedBox(height: 24),
+                sectionHeader("الفئات"),
+                Padding12,
+                //temp
+                //خله يطلع من البادنق
+                SizedBox(
+                  height: 95,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 8,
+                    separatorBuilder: (_, __) => const SizedBox(width: 12),
+                    itemBuilder: (context, index) => Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/framl.png',
+                          width: 74,
+                          height: 56,
+                        ),
+                        Padding8,
+                        Text(
+                          "الفرامل",
+                          style: AppTextStyles.mediumOverline.copyWith(
+                            color: AppColors.primaryText,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                dPadding,
+                sectionHeader("مناسب لسيارتي"),
+                Padding12,
+                GridView.builder(
+                  itemCount: 8,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    // childAspectRatio: 1,
+                    mainAxisExtent: 237, // This sets the fixed height
+                  ),
+                  itemBuilder: (context, index) => ProductCard(),
+                ),
+              ],
             ),
-            dPadding,
-            sectionHeader("مناسب لسيارتي"),
-            Padding12,
-            GridView.builder(
-              itemCount: 8,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                // childAspectRatio: 1,
-                mainAxisExtent: 237, // This sets the fixed height
-              ),
-              itemBuilder: (context, index) => ProductCard(),
+            Align(
+              alignment: AlignmentGeometry.bottomCenter,
+              child: CustomFloatingNavBar(),
             ),
           ],
         ),
